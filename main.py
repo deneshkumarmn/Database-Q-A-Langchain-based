24773b6df2dcb239f4124dbad2b31db0987e977e
Denesh Kumar Manoharan <83665847+deneshkumarmn@users.noreply.github.com>
2023-11-19 01:39:16 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,114 @@
+from langchain.llms import GooglePalm
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain.prompts import SemanticSimilarityExampleSelector
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.prompts import FewShotPromptTemplate
+from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
+from langchain.prompts.prompt import PromptTemplate
+from langchain.llms import GooglePalm
+
+api_key = "AIzaSyCFdl_lQLqM1RaPsU-o8PRyMDx_PSoDkXk"
+llm = GooglePalm(google_api_key = api_key, temperature = 0.4)
+
+db_user = "root"
+db_password = "deneshkumar"
+db_host = "localhost"
+db_name = "denesh_tshirts"
+db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
+
+db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+qns1 = db_chain.run("How many t-shirts do we have left for nike in extra small size and black color?")
+
+qns2 = db_chain.run("How much is the price of the inventory for all small size t-shirts?")
+qns2 = db_chain.run("SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'")
+
+sql_code = """select sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from (select sum(price*stock_quantity) as total_amount, t_shirt_id from t_shirts where brand = 'Levi'
+group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id"""
+qns3 = db_chain.run(sql_code)
+
+qns4 = db_chain.run("How much revenue can be made by selling all the levis t_shirt?")
+qns4 = db_chain.run("SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'")
+
+qns5 = db_chain.run("SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'")
+print("1")
+
+few_shots = [
+    {'Question' : "How many t-shirts do we have left for Nike in XS size and black color?",
+     'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Nike' AND color = 'White' AND size = 'XS'",
+     'SQLResult': "Result of the SQL query",
+     'Answer' : str(qns1)},
+    {'Question': "How much is the total price of the inventory for all S-size t-shirts?",
+     'SQLQuery':"SELECT SUM(price*stock_quantity) FROM t_shirts WHERE size = 'S'",
+     'SQLResult': "Result of the SQL query",
+     'Answer': str(qns2)},
+    {'Question': "If we have to sell all the Levi’s T-shirts today with discounts applied. How much revenue  our store will generate (post discounts)?" ,
+     'SQLQuery' : """SELECT sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from
+(select sum(price*stock_quantity) as total_amount, t_shirt_id from t_shirts where brand = 'Levi'
+group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_id
+ """,
+     'SQLResult': "Result of the SQL query",
+     'Answer': str(qns3)} ,
+     {'Question' : "If we have to sell all the Levi’s T-shirts today. How much revenue our store will generate without discount?" ,
+      'SQLQuery': "SELECT SUM(price * stock_quantity) FROM t_shirts WHERE brand = 'Levi'",
+      'SQLResult': "Result of the SQL query",
+      'Answer' : str(qns4)},
+    {'Question': "How many white color Levi's shirt I have?",
+     'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'",
+     'SQLResult': "Result of the SQL query",
+     'Answer' : str(qns5)
+     }
+]
+print("2")
+from langchain.prompts import SemanticSimilarityExampleSelector
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+import sentence_transformers
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+to_vectorize = [" ".join(example.values()) for example in few_shots]
+vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=few_shots)
+print("3")
+from langchain.prompts import FewShotPromptTemplate
+from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
+from langchain.prompts.prompt import PromptTemplate
+
+example_selector = SemanticSimilarityExampleSelector(vectorstore=vectorstore,k=2,)
+example_prompt = PromptTemplate(input_variables=["Question", "SQLQuery", "SQLResult","Answer",],
+    template="\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}",)
+
+mysql_prompt = """You are a MySQL expert. Given an input question, first create a syntactically correct MySQL query to run, then look at the results of the query and return the answer to the input question.
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per MySQL. You can order the results to return the most informative data in the database.
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+Pay attention to use CURDATE() function to get the current date, if the question involves "today".
+
+Use the following format:
+
+Question: Question here
+SQLQuery: Query to run with no pre-amble
+SQLResult: Result of the SQLQuery
+Answer: Final answer here
+
+No pre-amble.
+"""
+print("4")
+few_shot_prompt = FewShotPromptTemplate(
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix=mysql_prompt,
+    suffix=PROMPT_SUFFIX,
+    input_variables=["input", "table_info", "top_k"],
+)
+new_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, prompt=few_shot_prompt)
+q = new_chain.run('How much revenue  our store will generate by selling all Van Heuson TShirts without discount?')
+print("5")
+
+import streamlit as st
+st.title("DK T-Shirts: Database Q/A")
+question = st.text_input("Question: ")
+if question:
+    answer = new_chain.run(question)
+    st.header("Answer: ")
+    st.write(answer)
+
